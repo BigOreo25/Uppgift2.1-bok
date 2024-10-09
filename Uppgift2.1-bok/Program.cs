0fb3a24a4f99e8f6a4d2709c2411f5eaba570f5b
BigOreo25 <alts07001@utb.vaxjo.se>
2024-10-09 12:00:17 +0200

    Add project files.

---
+++ Uppgift2.1-bok/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace bok
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej igen! Vilken bok gillar du? Bara säg vad den heter.");
+            string bok = Console.ReadLine();
+            Console.WriteLine("Nice! " + bok + " är en bra bok asså.");
+
+        }
+
+    }
+
+}
